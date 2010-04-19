489406a2db057a31f8668a4427e0cdb224758ce0
cipibad <cipibad@57d18a92-91b9-0e65-4b07-8f3bbb02b405>
2010-04-19 21:28:18 +0000

    updated transmission startup script and added new rtorrent interface

--- scripts/optware/rtorrent-install.sh
+++ scripts/optware/rtorrent-install.sh
@@ -89,9 +89,34 @@ chmod +x /opt/etc/init.d/S90rtorrent
 echo rtorrent startup script prepared
 [ -f /opt/etc/rtorrent.conf ] && cp /opt/etc/rtorrent.conf /opt/etc/rtorrent.conf.old
 echo '
+min_peers = 5
+max_peers = 10
+max_uploads = 5
+
+
+download_rate = 512
+upload_rate = 32
+
+dht = auto
+encryption = allow_incoming,enable_retry,prefer_plaintext
+ip = home.cipex.ro
 directory = /tmp/hdd/volumes/HDD1/rtorrent/download
 session = /tmp/hdd/volumes/HDD1/rtorrent/config
+schedule = watch_directory,5,5,load_start=/tmp/hdd/volumes/HDD1/rtorrent/watch/*.torrent
+
 scgi_local = /tmp/rpc.socket
+
+#send_buffer_size = 4096
+#receive_buffer_size = 4096
+send_buffer_size = 4096
+receive_buffer_size = 4096
+
+max_memory_usage = 67108864
+#max_memory_usage = 134217728
+max_open_files = 128
+
+safe_sync = yes
+
 '> /opt/etc/rtorrent.conf
 
 
@@ -174,7 +199,29 @@ include "config.php.example";
 ?>
 EOF
 cd ..
+rm rtgui-0.2.7.tar
 
+# second web gui install
+cd /opt/share/www
+rm -rf rutorrent*
+wget http://rutorrent.googlecode.com/files/rutorrent-3.0.tar.gz
+/opt/bin/gunzip rutorrent-3.0.tar.gz
+tar xvf rutorrent-3.0.tar
+cd rutorrent/conf
+mv config.php config.php.dist
+cat > config.php <<EOF
+<?php
+
+include "config.php.dist";
+
+\$scgi_port = 0;
+\$scgi_host = "unix:///tmp/rpc.socket";
+?>
+EOF
+cd ../..
+rm rutorrent-3.0.tar
+
+sh /opt/etc/init.d/S90rtorrent start
 
 echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 echo !
