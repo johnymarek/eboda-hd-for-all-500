4dc3abac42e84e8eea71d8cd4887f04460033ba3
cipibad <cipibad@57d18a92-91b9-0e65-4b07-8f3bbb02b405>
2010-04-22 12:11:46 +0000

    some stepts fo HDD scripts integration

---
+++ trunk/scripts/feeds/scripts_vb6/scripts/php/220.php
@@ -0,0 +1,71 @@
+﻿<?php echo "<?phpxml version='1.0' ?>"; ?>
+<rss version="2.0" xmlns:dc="http://purl.org/dc/elements/1.1/">
+
+<channel>
+	<title>220.ro - selectii</title>
+	<menu>main menu</menu>
+
+
+<?php
+$query = $_GET["query"];
+if($query) {
+   $queryArr = explode(',', $query);
+   $page = $queryArr[0];
+   $search = $queryArr[1];
+}
+
+if($page) {
+    if($search) {
+        $html = file_get_contents("http://www.tube8.com/search.html?q=".$search."&page=".$page);
+    } else {
+        $html = file_get_contents("http://www.220.ro/".$page."");
+    }
+} else {
+    $page = 1;
+    if($search) {
+        $html = file_get_contents("http://www.tube8.com/search.html?q=".$search);
+    } else {
+        $html = file_get_contents("http://www.220.ro/");
+    }
+}
+
+
+$videos = explode('<div class="container_film_h">', $html);
+
+unset($videos[0]);
+$videos = array_values($videos);
+
+foreach($videos as $video) {
+    $t1 = explode('href="', $video);
+    $t2 = explode('"', $t1[1]);
+    $link = $t2[0];
+
+    $t1 = explode(' src="', $video);
+    $t2 = explode('"', $t1[1]);
+    $image = $t2[0];
+
+    $t1 = explode(' title="', $video);
+    $t2 = explode('"', $t1[1]);
+    $title = $t2[0];
+
+    $html = file_get_contents($link);
+    $t1 = explode('videoURL=', $html);
+    $t2 = explode('&preview', $t1[1]);
+    $link = urldecode($t2[0]);
+
+
+    echo '<item>';
+    echo '<title>'.$title.'</title>';
+    echo '<link>'.$link.'</link>';
+    echo '<media:thumbnail url="'.$image.'" />';
+    echo '<enclosure type="video/flv" url="'.$link.'"/>';	
+    echo '</item>';
+}
+
+
+?>
+
+
+
+</channel>
+</rss>
--- trunk/www/cgi/restore-webcontrol.cgi
+++ trunk/www/cgi/restore-webcontrol.cgi
@@ -2,16 +2,37 @@
 
 echo "Content-type: text/html"
 echo
-echo "<html><head><title>PlayOn!HD InfoSite</title></head><body bgcolor=\"#000000\" text=\"#FFFFFF\"><br><br><br>"
+echo "<html><head><title>Eboda Web Control Panel restore</title></head><body bgcolor=\"#000000\" text=\"#FFFFFF\"><br><br><br>"
 
-echo Eboda Web Control Panel restoring.
+tmpdir=/tmp
 
-recover
-{
-#    remove everything except backup and unpack-it
 
-}
-
+if [ -f ../ewcp-*.tar ] 
+then
+    echo old version was found
+    oldserial=`grep SERIAL $workdir/current_version.txt  | cut -d'=' -f 2` && echo old serial is $oldserial
+    tar cvf ${tmpdir}/ewcp-${oldserial}.tar * 
+    if [ $? -eq 0 ]
+    then
+        echo current version saved temporarly
+        rm -rf *
+        tar xvf /ewcp-*.tar
+        if [ $? -eq 0 ]
+        then
+            echo new version installed
+        else
+            echo cannot restore old version, trying to restore current version
+            tar xvf ${tmpdir}/ewcp-${oldserial}.tar
+            echo if panel not working anymore ... nashpa
+        fi
+    else
+        echo "cannot save current version in ${tmpdir}/ewcp-${oldserial}.tar"
+    fi
+else
+    echo "restore file not found"
+    
+fi
+echo '<FORM><INPUT TYPE="button" VALUE="Back" onClick="history.go(-1);return true;"> </FORM> '
 
 echo "<br></body></html>"
 
