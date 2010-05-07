841ea19bf99b6d4f7edcc12cd533b73a0620f41e
cipibad <cipibad@57d18a92-91b9-0e65-4b07-8f3bbb02b405>
2010-05-07 21:35:36 +0000

    new lighttpd

--- trunk/3pp/lighttpd/build.sh
+++ trunk/3pp/lighttpd/build.sh
@@ -1,8 +1,1 @@
-#dynamic
-
-./configure --prefix=/opt --host=mipsel-linux --disable-ipv6 --disable-lfs --without-mysql --without-ldap --without-attr --without-valgrind --without-openssl --without-kerberos5 --without-pcre --without-zlib --without-bzip2 --without-webdav-props --without-webdav-locks --without-gdbm --without-memcache --without-lua
-
-
-#static
-
-./configure --prefix=/opt --host=mipsel-linux --disable-ipv6 --disable-lfs --without-mysql --without-ldap --without-attr --without-valgrind --without-openssl --without-kerberos5 --without-pcre --without-zlib --without-bzip2 --without-webdav-props --without-webdav-locks --without-gdbm --without-memcache --without-lua --enable-static
+./configure --prefix=/opt --host=mipsel-linux --disable-ipv6 --without-mysql --without-ldap --without-valgrind --without-openssl --without-kerberos5 --with-pcre --without-zlib --without-bzip2
