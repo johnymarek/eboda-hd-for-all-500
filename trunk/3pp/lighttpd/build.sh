b45236c80cd79da9d51dbac09d91fcaeb6383fe8
cipibad <cipibad@57d18a92-91b9-0e65-4b07-8f3bbb02b405>
2010-05-08 21:35:54 +0000

    3pp removed

--- trunk/3pp/lighttpd/build.sh
+++
@@ -1,1 +0,0 @@
-./configure --prefix=/opt --host=mipsel-linux --disable-ipv6 --without-mysql --without-ldap --without-valgrind --without-openssl --without-kerberos5 --with-pcre --without-zlib --without-bzip2
