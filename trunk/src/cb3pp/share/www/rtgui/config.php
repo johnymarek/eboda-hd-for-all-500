f2382463d81b3afabbd0c23ef7a163200c251ff1
cipibad <cipibad@57d18a92-91b9-0e65-4b07-8f3bbb02b405>
2010-05-15 18:12:18 +0000

    cleanup rtorrent replaced by btpd 0.15

--- trunk/src/cb3pp/share/www/rtgui/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-include "config.php.example";
-
-// Connect string for your local RPC/rTorrent connection:
-$rpc_connect="http://localhost:8081/RPC2";
-
-?>
--- trunk/src/cb3pp/share/www/rutorrent/conf/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-include "config.php.dist";
-
-$scgi_port = 0;
-$scgi_host = "unix:///tmp/rpc.socket";
-?>
