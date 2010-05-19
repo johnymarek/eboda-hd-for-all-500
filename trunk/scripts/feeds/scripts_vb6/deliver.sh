19c0ad1d0ed30de131dd6fd002cb38f6a68b7359
cipibad <cipibad@57d18a92-91b9-0e65-4b07-8f3bbb02b405>
2010-05-19 20:23:19 +0000

    scripts update to cb3pp path

--- trunk/scripts/feeds/scripts_vb6/deliver.sh
+++ trunk/scripts/feeds/scripts_vb6/deliver.sh
@@ -5,6 +5,12 @@
 
 #deliver menu
 
-touch scripts/.modified_full_firmware2
-find scripts | grep -v .svn | zip -9 scripts-latest.zip -@
-rm scripts/.modified_full_firmware2
+rm scripts-latest.zip
+
+find scripts | grep -v .svn |  grep -v ~$ | zip -9 scripts-latest.zip -@
+
+grep '=' scripts/scripts-version.txt | sed -e 's/^/LATEST_/'  > scripts-latest-version.txt
+
+googlecode_upload.py -s scripts -p eboda-hd-for-all-500 scripts-latest.zip -u cipibad -w WV6tc7mt3Ba8
+
+googlecode_upload.py -s scripts -p eboda-hd-for-all-500 scripts-latest-version.txt -u cipibad -w WV6tc7mt3Ba8
