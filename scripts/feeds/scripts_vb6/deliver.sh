174402b647aed56185da6a5b082f9180d4cf1f33
cipibad <cipibad@57d18a92-91b9-0e65-4b07-8f3bbb02b405>
2010-09-11 17:06:40 +0000

    more cleaning

--- scripts/feeds/scripts_vb6/deliver.sh
+++ scripts/feeds/scripts_vb6/deliver.sh
@@ -9,8 +9,6 @@ rm scripts-latest.zip
 
 find scripts | grep -v .svn |  grep -v ~$ | zip -9 scripts-latest.zip -@
 
-grep '=' scripts/scripts-version.txt | sed -e 's/^/LATEST_/'  > scripts-latest-version.txt
-
 googlecode_upload.py -s scripts -p eboda-hd-for-all-500 scripts-latest.zip -u cipibad -w WV6tc7mt3Ba8
 
-googlecode_upload.py -s scripts -p eboda-hd-for-all-500 scripts-latest-version.txt -u cipibad -w WV6tc7mt3Ba8
+googlecode_upload.py -s scripts -p eboda-hd-for-all-500 scripts-version.txt -u cipibad -w WV6tc7mt3Ba8
