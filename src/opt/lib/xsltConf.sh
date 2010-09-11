a99bf3090e859a89403e766cb4e39034eb1b30bc
cipibad <cipibad@57d18a92-91b9-0e65-4b07-8f3bbb02b405>
2010-09-11 17:05:29 +0000

    cleaning

--- src/opt/lib/xsltConf.sh
+++
@@ -1,7 +0,0 @@
-#
-# Configuration file for using the xslt library
-#
-XSLT_LIBDIR="-L/opt/lib"
-XSLT_LIBS="-lxslt -L/home/slug/optware/oleg/staging/opt/lib -L/home/slug/optware/oleg/staging/opt/lib -lxml2 -lz -lm -lm"
-XSLT_INCLUDEDIR="-I/opt/include"
-MODULE_VERSION="xslt-1.1.26"
